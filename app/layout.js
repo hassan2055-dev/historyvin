b26485efc79998e8e65da359be52b48260540d02
zafar205 <mz09206@st.habib.edu.pk>
2025-12-12 20:35:15 +0500

    hjh

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "HistorivinStore - #1 Vehicle History Reports | HistoriVIN Car Reports",
@@ -319,9 +308,7 @@ export default function RootLayout({ children }) {
           }}
         />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         {children}
       </body>
     </html>
